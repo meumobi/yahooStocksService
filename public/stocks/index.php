ab342f978f49cc611b4558d673d0e1cd0762b73b
Victor Dias <victor.dias@CSE-MOBI-CL.Cloud.local>
2014-05-22 20:44:57 -0300

    unset display errors

--- public/stocks/index.php
+++ public/stocks/index.php
@@ -3,7 +3,7 @@ require_once '../vendor/autoload.php';
 require_once "../libs/phpfastcache/phpfastcache.php";
 require_once '../services/StockService.php';
 
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
 
 $action = isset($_GET['action']) ? $_GET['action'] : null;
 $stockService = new StockService();
