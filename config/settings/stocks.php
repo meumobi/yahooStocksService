97ec57f3780773c1fa3671c9d5d7aa4f09ed5918
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-08-15 12:08:56 -0300

    update config

--- config/settings/stocks.php
+++ config/settings/stocks.php
@@ -3,7 +3,7 @@ require_once "services/Service.php";
 require_once 'services/StockService.php';
 
 define('CACHE_TIME', 60 * 15); //15 minutes
-define('CACHE_PATH', APP_ROOT . '/tmp/cache/redirect');
+define('CACHE_PATH', APP_ROOT . '/tmp/cache/stocks');
 define('REQUEST_TIMEOUT', 10);
 define('ENFOQUE_URL', 'http://webservice.enfoque.com.br/wsbancosantander/cotacoes.asmx/Tabela');	
 define('YAHOO_URL', 'https://query.yahooapis.com/v1/public/yql');	
